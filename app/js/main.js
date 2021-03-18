7d522fdcfe23e09d347bf428821022c17749b30b
YouLiaKovalchuk <ullleeenn@gmail.com>
2021-03-18 14:04:57 +0200

    added goods section, added design section

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 $(function () {
-
   $('.home__slider').slick({
     arrows: false,
-    dots: true
+    dots: true,
+    fade: true,
+    // autoplay: true,
+    autoplaySpeed: 2000
   })
 
   mixitup('.products__cards')
