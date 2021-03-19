046024fa475c5f9c0a5eff11617afb3141a3c829
YouLiaKovalchuk <ullleeenn@gmail.com>
2021-03-19 13:48:14 +0200

    fixed sections, added design section, added insights

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,17 @@ $(function () {
     arrows: false,
     dots: true,
     fade: true,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000
-  })
+  });
+
+  $('.partners__carousel').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow: 5,
+    slidesToScroll: 1
+  });
+
 
   mixitup('.products__cards')
 });
