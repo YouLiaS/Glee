f70e4894025b69f4b0a983b64ab19cb4760d07b3
YouLiaS <68943740+YouLiaS@users.noreply.github.com>
2021-03-19 16:12:29 +0200

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,17 @@
 $(function () {
-
   $('.home__slider').slick({
     arrows: false,
-    dots: true
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  })
+
+  $('.partners__carousel').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow: 5,
+    slidesToScroll: 1
   })
 
   mixitup('.products__cards')
